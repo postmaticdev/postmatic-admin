52301a0d2cecf77da4a12ff24c170208acf1e4f9
lanstheprodigy <alan.040219.gam@gmail.com>
2026-07-02 13:15:59 +0700

    Add NEXT_PUBLIC_ADMIN_ORIGIN and update LOGIN_URL to include admin origin

--- constants/index.ts
+++ constants/index.ts
@@ -2,7 +2,8 @@ export const NEXT_PUBLIC_API_ORIGIN =
   process.env.NEXT_PUBLIC_API_ORIGIN || "https://api-staging.postmatic.id";
 export const NEXT_PUBLIC_AUTH_ORIGIN =
   process.env.NEXT_PUBLIC_AUTH_ORIGIN || "https://auth-staging.postmatic.id";
+export const NEXT_PUBLIC_ADMIN_ORIGIN = "https://admin-haystudio-staging.postmatic.id"
 
 export const ACCESS_TOKEN_KEY = "postmaticAccessToken";
 export const REFRESH_TOKEN_KEY = "postmaticRefreshToken";
-export const LOGIN_URL = NEXT_PUBLIC_AUTH_ORIGIN;
+export const LOGIN_URL = NEXT_PUBLIC_AUTH_ORIGIN + "/login" + "?from=" + NEXT_PUBLIC_ADMIN_ORIGIN;
